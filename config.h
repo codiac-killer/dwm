d89fbc2b16ddc38cd77e8f2961dada306b50173f
Kostas Kontaxis <kostaskontaxis360@gmail.com>
2020-11-29 00:43:33 +0200

    Systray

--- config.h
+++ config.h
@@ -5,19 +5,25 @@
 // #############################################################################
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const int CORNER_RADIUS 		= 3;		/* radius for rounded corners on clients */
 static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 35;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Cantarell:style=Regular:size=10" };
+static const char dmenufont[]       = "Cantarell:style=Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_nord_red[]	= "#BF616A";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -34,10 +40,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      		instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  		NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Brave-browser",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Code",  			NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Gimp",     		NULL,       NULL,       1 << 2,       1,           -1 },
+	{ "Firefox",  		NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Brave-browser",	NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "subl",  			NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -68,7 +74,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] 			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  			= { "alacritty", NULL };
 static const char *fmcmd[]  			= { "thunar", NULL };
-static const char *browsercmd[]  		= { "brave-browser", NULL };
+static const char *browsercmd[]  		= { "brave", NULL };
 static const char *raise_volumecmd[]  	= { "amixer", "-D", "pulse", "sset", "Master", "5%+", NULL };
 static const char *lower_volumecmd[]  	= { "amixer", "-D", "pulse", "sset", "Master", "5%-", NULL };
 static const char *mute_volumecmd[]  	= { "amixer", "-D", "pulse", "sset", "Master", "toggle", NULL };
@@ -93,11 +99,11 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, 					zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    					view,           {0} },
 	{ MODKEY,             			XK_x,      					killclient,     {0} },
-	{ MODKEY,                       XK_t,      					setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      					setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      					setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  					setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  					togglefloating, {0} },
+	// { MODKEY,                       XK_t,      					setlayout,      {.v = &layouts[0]} },
+	// { MODKEY,                       XK_f,      					setlayout,      {.v = &layouts[1]} },
+	// { MODKEY,                       XK_m,      					setlayout,      {.v = &layouts[2]} },
+	// { MODKEY,                       XK_space,  					setlayout,      {0} },
+	{ MODKEY,             			XK_f,  						togglefloating, {0} },
 	{ MODKEY,                       XK_0,      					view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      					tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  					focusmon,       {.i = -1 } },
@@ -115,8 +121,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      	8)
 	{ MODKEY|ShiftMask,             XK_q,      					quit,           {0} },
 	{ 0,                       		XF86XK_AudioRaiseVolume,	spawn,          {.v = raise_volumecmd } },
-	{ 0,                       		XF86XK_AudioMute,      		spawn,          {.v = lower_volumecmd } },
-	{ 0,                       		XF86XK_AudioRaiseVolume,	spawn,          {.v = mute_volumecmd } },
+	{ 0,                       		XF86XK_AudioLowerVolume,	spawn,          {.v = lower_volumecmd } },
+	{ 0,                       		XF86XK_AudioMute,			spawn,          {.v = mute_volumecmd } },
 	{ 0,                       		XF86XK_AudioPlay,      		spawn,          {.v = media_playcmd } },
 	{ 0,                       		XF86XK_AudioStop,      		spawn,          {.v = media_stopcmd } },
 	{ 0,                       		XF86XK_AudioPrev,      		spawn,          {.v = media_nextcmd } },
@@ -133,6 +139,7 @@ static Button buttons[] = {
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	// { ClkClientWin,         MODKEY,         Button1,        tilemovemouse, 	{0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
