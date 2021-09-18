04538e7bc07c36e5b9adfee37ed27265d81158d6
Kostas Kontaxis <kkontaxis@pm.me>
2021-09-18 10:25:02 +0300

    Changed dmenu to default app-launch + changed its style

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 35;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int uline_thickness    = 3;        // thickness of underline of selected tag(s)
 static const char *fonts[]          = { "Cantarell:style=Regular:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Cantarell:style=Regular:size=10";
+static const char dmenufont[]       = "Cantarell:style=Regular:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -97,7 +97,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]           = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_nord_gray, "-nf", col_nord_white, "-sb", col_nord_green, "-sf", col_nord_gray, "-c", "-l", "10", NULL };
+static const char *dmenucmd[]           = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-l", "20", NULL };
 static const char *roficmd[]            = { "rofi", "-show", "drun", "-modi", "drun", "-theme", "oneDark", NULL };
 static const char *termcmd[]            = { "st", "mksh", NULL };
 static const char *fmcmd[]              = { "st", "ranger", NULL };
