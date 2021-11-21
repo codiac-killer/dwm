40f867fe25a100bea44ec19187b6f90911d49464
Kostas Kontaxis <kkontaxis@pm.me>
2021-11-21 08:48:25 +0200

    [minor] Chagned status bar font to bold

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 35;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int uline_thickness    = 3;        // thickness of underline of selected tag(s)
-static const char *fonts[]          = { "Cantarell:style=Regular:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Cantarell:style=Bold:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Cantarell:style=Regular:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -99,7 +99,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]           = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-l", "20", NULL };
 static const char *roficmd[]            = { "rofi", "-show", "drun", "-modi", "drun", "-theme", "oneDark", NULL };
-static const char *termcmd[]            = { "st", NULL };
+static const char *termcmd[]            = { "st", "zsh", NULL };
 static const char *fmcmd[]              = { "st", "-e", "sfm", NULL };
 static const char *browsercmd[]         = { "qutebrowser", NULL};
 // static const char *raise_volumecmd[]     = { "amixer", "-D", "pulse", "sset", "Master", "5%+;", "kill", "-44", "$(pidof", "dwmblocks)", NULL };
